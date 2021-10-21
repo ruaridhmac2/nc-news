317e3cd1980926e0b9bdf35523b26aed59fb6ca9
Ruaridh Webster <Ruaridh.webster@arup.com>
2021-10-21 17:08:31 +0100

    some styling updates

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -16,19 +16,17 @@ function Comments({ comments, setComments }) {
   }, []);
 
   return (
-    <section>
-      <ul className="comment-list">
-        {comments.map((comment) => {
-          return (
-            <div key={comment.created_at + comment.author} className="comment">
-              <p>{comment.author}</p>
-              <p>{comment.created_at}</p>
-              <p>{comment.body}</p>
-            </div>
-          );
-        })}
-      </ul>
-    </section>
+    <ul className="comment-list">
+      {comments.map((comment) => {
+        return (
+          <div key={comment.created_at + comment.author} className="comment">
+            <p>{comment.author}</p>
+            <p>{comment.created_at}</p>
+            <p>{comment.body}</p>
+          </div>
+        );
+      })}
+    </ul>
   );
 }
 
